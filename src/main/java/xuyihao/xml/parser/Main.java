9aa9b699dd3a8b9436f7d592c1b01bbbb1399394
徐怡浩 <xuyh@uyunsoft.cn>
2017-01-12 17:17:56 +0800

    initial commit

---
+++ src/main/java/xuyihao/xml/parser/Main.java
@@ -0,0 +1,11 @@
+package xuyihao.xml.parser;
+
+/**
+ * 
+ * @Author Xuyh created at 2017年1月12日 下午4:25:12
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+	}
+}
---
+++ src/test/java/xuyihao/xml/parser/MainTest.java
@@ -0,0 +1,19 @@
+package xuyihao.xml.parser;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @Author Xuyh created at 2017年1月12日 下午4:25:56
+ *
+ */
+public class MainTest {
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+}
